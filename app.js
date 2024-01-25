71bd4e2f1e04881eafcecdece77aeb011f534c4f
Pritish Poswal <77208293+pritishposwal@users.noreply.github.com>
2024-01-25 12:23:18 +0530

    Update database to mysql2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const app = express();
